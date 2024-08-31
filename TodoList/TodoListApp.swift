96f42db51930a00b7e2ad9c71bf40634383d9df8
Shaileshhariharan03 <57766024+Shaileshhariharan03@users.noreply.github.com>
2024-08-31 23:27:52 +0530

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Shailesh H on 31/08/24.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
